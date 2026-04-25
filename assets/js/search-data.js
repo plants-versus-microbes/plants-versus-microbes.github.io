b84af9053f10355c23dfef3b7ce744b665e775ef
plants-versus-microbes <crfitzpat@gmail.com>
2026-04-25 02:42:26 +0000

    Deploying to gh-pages from @ plants-versus-microbes/plants-versus-microbes.github.io@ad83edaf6f2af661e2da2c6fa051fc72401972c8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,12 +37,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-research",
+          title: "research",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/research/";
           },
         },{id: "nav-publications",
           title: "publications",
